209d2664b0775246cd726c4efa96fdb6e96d8f08
rohit-285 <rohitsharma28205@gmail.com>
2026-05-03 13:05:49 +0530

    Updated database config

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/boozemeter";
-    const conn = await mongoose.connect(uri);
+    const conn = await mongoose.connect(
+      process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/alcoaware'
+    );
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Database Error: ${error.message}`);
     process.exit(1);
   }
 };
