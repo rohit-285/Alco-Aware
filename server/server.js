8058288fca93a7b44b679ac9876b4f21b32d4f4f
rohit-285 <rohitsharma28205@gmail.com>
2026-05-03 22:03:00 +0530

    Fixed CORS issue

--- server/server.js
+++ server/server.js
@@ -14,7 +14,10 @@ const app = express();
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
