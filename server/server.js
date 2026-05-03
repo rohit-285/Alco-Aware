c8ac70cb06e9955969a7bc16f8de275e5820fa4d
rohit-285 <rohitsharma28205@gmail.com>
2026-05-03 22:03:49 +0530

    Fixed CORS issue

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: process.env.https://alco-aware.vercel.app/,
   credentials: true
 }));
 app.use(compression());
